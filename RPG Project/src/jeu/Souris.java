981152d3c67e69aea8f30db74c36bac293205cec
Twarz <cortacero.k31130@gmail.com>
2015-06-15 01:36:51 +0200

    mise à jour image souris

---
+++ RPG Project/src/jeu/Souris.java
@@ -0,0 +1,12 @@
+package jeu;
+
+import javax.swing.ImageIcon;
+
+public class Souris {
+	
+	private ImageIcon image;
+	
+	public Souris(){
+		this.image = new ImageIcon("images/souris.jpg");
+	}
+}
