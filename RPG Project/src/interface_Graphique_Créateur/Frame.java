8ecafd013223d25249cc34aed593fa64220167e7
Twarz <cortacero.k31130@gmail.com>
2015-06-14 19:25:43 +0200

    Mise à jour

--- RPG Project/src/interface_Graphique_Créateur/Frame.java
+++ RPG Project/src/interface_Graphique_Créateur/Frame.java
@@ -1,27 +1,18 @@
 package interface_Graphique_Créateur;
 
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class Frame extends JFrame{
-	
-	private DimensionEcran dimensionEcran;
-	private PanelCréateur panelPrincipal;
-//	private PanelChoixObjetsCréateur panelChoixObjets;
 
 	public Frame(){
 		super("Création de niveau");
-		this.dimensionEcran = new DimensionEcran();
-		this.setSize(this.dimensionEcran.getSize());
+		this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setExtendedState(MAXIMIZED_BOTH);
-		
-		
-		this.panelPrincipal = new PanelCréateur(this.dimensionEcran);
-	//	this.panelChoixObjets = new PanelChoixObjetsCréateur(this.dimensionEcran);
-		
-		this.add(this.panelPrincipal);
-		//this.add(this.panelChoixObjets);
+		this.add(new PanelCréateur());
 	}
 }
--- RPG Project/src/interface_Graphique_Créateur/PanelCréateur.java
+++ RPG Project/src/interface_Graphique_Créateur/PanelCréateur.java
@@ -1,20 +1,22 @@
 package interface_Graphique_Créateur;
 
 import java.awt.Color;
+import java.awt.Toolkit;
+
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class PanelCréateur extends JPanel{
 	
-	public PanelCréateur(DimensionEcran dimensionEcran){
+	public PanelCréateur(){
 		super(null);
 		this.setLayout(null);
 		this.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1),	
 					   BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.BLUE), 			
 					   BorderFactory.createEmptyBorder(10, 10, 10, 10))));
 		this.setBackground(new Color(220,220,220));
-		this.setBounds(0, 0,dimensionEcran.width, dimensionEcran.height);
-		this.add(new PanelChoixObjetsCréateur(dimensionEcran));
+		this.setBounds(0, 0,Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
+		this.add(new PanelChoixObjetsCréateur());
 	}
 }
