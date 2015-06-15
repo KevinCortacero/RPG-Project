8e4fd67209200679b7ae792d4b5dd3c4d4b0d26d
Twarz <cortacero.k31130@gmail.com>
2015-06-15 19:42:23 +0200

    RAS

--- RPG Project/src/interface_Graphique_Créateur/PanelGestionCréateur.java
+++ RPG Project/src/interface_Graphique_Créateur/PanelGestionCréateur.java
@@ -6,7 +6,6 @@ import java.awt.Toolkit;
 import java.io.File;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 
@@ -33,61 +32,48 @@ public class PanelGestionCréateur extends JPanel {
 
 	private void listRoot() {
 		this.racine = new DefaultMutableTreeNode("Liste des cartes :");
-		int count = 0;
 
 		for (String file : this.carte.list()) {
 
-			System.out.println(this.carte.getAbsolutePath() + "\\" + file);
-			
-     		DefaultMutableTreeNode lecteur = new DefaultMutableTreeNode(file); 
-     		
+			DefaultMutableTreeNode lecteur = new DefaultMutableTreeNode(file);
 			File fileEnfant = new File(this.carte.getAbsolutePath()+ "\\" + file);
-			if ( fileEnfant.isDirectory()){
-				try { 
-					 for(String nom : fileEnfant.list()){ 
-						 DefaultMutableTreeNode node = new DefaultMutableTreeNode(nom );
-						 lecteur.add(this.listFile( new File( this.carte.getAbsolutePath() + "\\" + file + "\\" + nom), node  ));
-						 }
-					 } catch
-					 (NullPointerException e) {}
-			}
 
+			if ( fileEnfant.isDirectory()){
+				for(String nom : fileEnfant.list()){ 
+					DefaultMutableTreeNode node = new DefaultMutableTreeNode(nom);
+					lecteur.add(this.listFile( new File( this.carte.getAbsolutePath() + "\\" + file + "\\" + nom), node  ));
+				}
+			}
 			this.racine.add(lecteur);
 		}
-		// Nous créons, avec notre hiérarchie, un arbre
-		arbre = new JTree(this.racine);
-		// Que nous plaçons sur le ContentPane de notre JFrame à l'aide d'un
-		// scroll
-		this.add(new JScrollPane(arbre));
+
+		this.arbre = new JTree(this.racine);
+		this.arbre.setBackground(new Color(245, 245, 245));
+		this.add(arbre);
 	}
 
-	private DefaultMutableTreeNode listFile(File file,
-			DefaultMutableTreeNode node) {
-		int count = 0;
+	private DefaultMutableTreeNode listFile(File file, DefaultMutableTreeNode node) {
 
 		if (file.isFile())
 			return new DefaultMutableTreeNode(file.getName());
-		else {
-			File[] list = file.listFiles();
-			if (list == null)
-				return new DefaultMutableTreeNode(file.getName());
 
-			for (File nom : list) {
-				count++;
-				// Pas plus de 5 enfants par noeud
-				if (count < 5) {
-					DefaultMutableTreeNode subNode;
-					if (nom.isDirectory()) {
-						subNode = new DefaultMutableTreeNode(nom.getName()
-								+ "\\");
-						node.add(this.listFile(nom, subNode));
-					} else {
-						subNode = new DefaultMutableTreeNode(nom.getName());
-					}
-					node.add(subNode);
-				}
+		File[] list = file.listFiles();
+		if (list == null)
+			return new DefaultMutableTreeNode(file.getName());
+
+		for (File nom : list) {
+			DefaultMutableTreeNode subNode;
+			if (nom.isDirectory()) {
+				subNode = new DefaultMutableTreeNode(nom.getName()
+						+ "\\");
+				node.add(this.listFile(nom, subNode));
+			} else {
+				subNode = new DefaultMutableTreeNode(nom.getName());
 			}
-			return node;
+			node.add(subNode);
 		}
+
+		return node;
 	}
 }
+
