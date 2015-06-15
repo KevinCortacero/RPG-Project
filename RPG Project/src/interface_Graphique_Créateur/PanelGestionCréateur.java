391e174c92ff4509dd485c66e29d700a0db89676
Twarz <cortacero.k31130@gmail.com>
2015-06-15 17:55:29 +0200

    push pour Pierre

--- RPG Project/src/interface_Graphique_Créateur/PanelGestionCréateur.java
+++ RPG Project/src/interface_Graphique_Créateur/PanelGestionCréateur.java
@@ -3,14 +3,22 @@ package interface_Graphique_Créateur;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Toolkit;
+import java.io.File;
+import java.nio.file.FileSystem;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 @SuppressWarnings("serial")
 public class PanelGestionCréateur extends JPanel{
 
 	private JLabel titre;
+	private JTree arbre;
+	private FileSystem file;
+	private DefaultMutableTreeNode racine;
 
 	public PanelGestionCréateur(){
 		super();
@@ -20,6 +28,59 @@ public class PanelGestionCréateur extends JPanel{
 		this.titre = new JLabel("Gestion des cartes");
 		this.titre.setFont(new Font("Arial", 18,18));
 		this.add(this.titre);
-		this.add(new JTree());
+		this.listRoot();
+	}
+
+	private void listRoot(){      
+		this.racine = new DefaultMutableTreeNode("Liste des cartes :");       
+		int count = 0;
+		
+		
+		for(Folder file : java.lang.Object[].){
+			
+			
+			
+			DefaultMutableTreeNode lecteur = new DefaultMutableTreeNode(file.getAbsolutePath());
+			try {
+				for(File nom : file.listFiles()){
+					DefaultMutableTreeNode node = new DefaultMutableTreeNode(nom.getName()+"\\");               
+					lecteur.add(this.listFile(nom, node));               
+				}
+			} catch (NullPointerException e) {}
+
+			this.racine.add(lecteur);                 
+		}
+		//Nous créons, avec notre hiérarchie, un arbre
+		arbre = new JTree(this.racine);      
+		//Que nous plaçons sur le ContentPane de notre JFrame à l'aide d'un scroll 
+		this.add(new JScrollPane(arbre));
+	}
+
+	private DefaultMutableTreeNode listFile(File file, DefaultMutableTreeNode node){
+		int count = 0;
+
+		if(file.isFile())
+			return new DefaultMutableTreeNode(file.getName());
+		else{
+			File[] list = file.listFiles();
+			if(list == null)
+				return new DefaultMutableTreeNode(file.getName());
+
+			for(File nom : list){
+				count++;
+				//Pas plus de 5 enfants par noeud
+				if(count < 5){
+					DefaultMutableTreeNode subNode;
+					if(nom.isDirectory()){
+						subNode = new DefaultMutableTreeNode(nom.getName()+"\\");
+						node.add(this.listFile(nom, subNode));
+					}else{
+						subNode = new DefaultMutableTreeNode(nom.getName());
+					}
+					node.add(subNode);
+				}
+			}
+			return node;
+		}
 	}
 }
