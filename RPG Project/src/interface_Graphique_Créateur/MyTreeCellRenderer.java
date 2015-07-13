bbbc53433b8f454888ee493eb9606694e28d4b99
Twarz <cortacero.k31130@gmail.com>
2015-07-14 01:37:42 +0200

    ras

--- RPG Project/src/interface_Graphique_Créateur/MyTreeCellRenderer.java
+++ RPG Project/src/interface_Graphique_Créateur/MyTreeCellRenderer.java
@@ -8,24 +8,24 @@ import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
 public class MyTreeCellRenderer extends DefaultTreeCellRenderer {
-	 
+
 	public Component getTreeCellRendererComponent(JTree tree, Object value,
-			  boolean sel,boolean expanded, boolean leaf, int row, boolean hasFocus) {
-			 
-			    setText(value.toString() );
-			    setForeground(Color.BLACK);
-			    setFont(new Font("Courrier", Font.PLAIN ,12));
-			    if(sel) {
-			    	
-			        if(value.toString().contains(".txt")){
-			        	setFont(new Font("Courrier", Font.BOLD ,12));
-				        setForeground(new Color(111,0,50)); //Ou la couleur que tu veux
-				    }else{
-				    	setFont(new Font("Courrier", Font.BOLD ,12));
-				        setForeground(new Color(50,0,111)); //Ou la couleur que tu veux
-				    }
-			    }
-			    
-			    return this;
-			  }
+			boolean sel,boolean expanded, boolean leaf, int row, boolean hasFocus) {
+
+		setText(value.toString() );
+		setForeground(Color.BLACK);
+		setFont(new Font("Courrier", Font.PLAIN ,12));
+		this.setBackgroundNonSelectionColor(new Color(245, 245, 245));
+		if(sel) {
+
+			if(value.toString().contains(".txt")){
+				setFont(new Font("Courrier", Font.BOLD ,12));
+				setForeground(new Color(110,0,50)); //Ou la couleur que tu veux
+			}else{
+				setFont(new Font("Courrier", Font.BOLD ,12));
+				setForeground(new Color(50,0,110)); //Ou la couleur que tu veux
+			}
+		}
+		return this;
+	}
 }
