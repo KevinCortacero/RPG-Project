6f61aef9848cd39ec77bdb77f3f51638ea1c2614
Twarz <cortacero.k31130@gmail.com>
2016-02-17 08:10:14 +0100

    ras

--- Cryptographie/CalculInverse.py
+++ Cryptographie/CalculInverse.py
@@ -5,22 +5,24 @@ Created on 16 f�vr. 2016
 '''
 
 
-def Fermat(x,p):
-    print("Calcul de l'inverse de {0} par Fermat, dans Z/{1}".format(x, p))
-    print("On cherche donc x^(p-2) = {0}^({1})".format(x,p-2))
-    mod = p-2
-    exposant = 0
-    inverse = 0
+def Fermat(x,p,n):
+    print("Calcul de l'inverse de {0} par Fermat, dans Z/{1}".format(x, n))
+    print("On cherche donc x^(p-2) = {0}^({1})".format(x,p))
+    mod = p
+    valeur = x
+    compteur = inverse = 1
     while(mod != 0) :
         print (mod % 2)
+        print("{0}^{1} = {2} ({3})".format(x,compteur,valeur,n))
         if (mod % 2 == 1):
-            inverse = (inverse + x**(2**exposant)) % p-2
+            inverse *= valeur % (n)
         mod = int(mod/2)
-        exposant +=1
-    print("{0}^-1 = {1} dans Z/{2}".format(x,inverse,p))  
-    print(x**(p-2) % p)
+        valeur = (valeur**2) % (n)
+        compteur *= 2
+    print("{0}^-1 = {1} dans Z/{2}".format(x,inverse % (p),p))  
+    print(x**(p) % n)
     
 if __name__ == '__main__':
-    Fermat(7, 13)
+    Fermat(12044, 2, 23449)
 
 
