00e8aa12beafe54fecb143951985a4a4c34b2756
twarz <cortacero.gamer@gmail.com>
2016-02-18 22:03:53 +0100

    cheaté

--- Cryptographie/nombrePremier.py
+++ Cryptographie/nombrePremier.py
@@ -15,13 +15,14 @@ def test(l, nombre):
 if __name__ == '__main__' :
     l = list()
     l.append(2)
-    nombre = 2
+    nombre = 3
+    nombre = int(nombre)
     print("go")
     while l.__len__() < 100000 :
         if test(l, nombre) :
             print(nombre)
             l.append(nombre)
-        nombre += 1
+        nombre += 2
     print (l)
    
             
--- Mathématiques/src/nombresPremiers.java
+++ Mathématiques/src/nombresPremiers.java
@@ -3,7 +3,7 @@ public class nombresPremiers {
 
 	public static void main(String[] args) {
 		int nombres[] = new int[1000000];
-		int nombre = 2;
+		int nombre = 3;
 		nombres[0] = 2;
 		int compteurNBPremier = 1;
 		
@@ -11,14 +11,16 @@ public class nombresPremiers {
 			if (testerNombre(nombres, nombre, compteurNBPremier)){
 				nombres[compteurNBPremier] = nombre;
 				compteurNBPremier ++;
-				System.out.println(nombre + "        -----> Je vais en acheter un !!!");
+				System.out.println(nombre);
 			}
-			nombre ++;
+			nombre +=2;
 		}
 	}
 	
 	private static boolean testerNombre(int nombres[], int nombre, int max){
-		for (int i = 0; i < max; i ++){
+		for (int i = 0; i < max; i ++ ){
+			if (nombres[i] > (int)Math.sqrt(nombre))
+				break;
 			if (nombre % nombres[i] == 0)
 				return false;
 		}
