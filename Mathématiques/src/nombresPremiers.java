4f97c38a6d2ede330d5d46fc7edb9a20918e4b17
twarz <cortacero.gamer@gmail.com>
2016-02-18 23:15:12 +0100

    ras

--- Mathématiques/src/nombresPremiers.java
+++ Mathématiques/src/nombresPremiers.java
@@ -2,7 +2,7 @@
 public class nombresPremiers {
 
 	public static void main(String[] args) {
-		int nombres[] = new int[1000000];
+		int nombres[] = new int[100];
 		int nombre = 3;
 		nombres[0] = 2;
 		int compteurNBPremier = 1;
@@ -11,9 +11,14 @@ public class nombresPremiers {
 			if (testerNombre(nombres, nombre, compteurNBPremier)){
 				nombres[compteurNBPremier] = nombre;
 				compteurNBPremier ++;
-				System.out.println(nombre);
 			}
 			nombre +=2;
+		}
+		for (int i = 0; i < nombres.length; i ++ ){
+			String nbs = String.valueOf(nombres[i]);
+			String cs = String.valueOf(nbs.charAt(nbs.length()-1));
+			int c = Integer.valueOf(cs);
+			System.out.println("[" + (i+1) + "] --> " + nbs + " | " + c + " | " + (nombres[i]%c) + " | ");
 		}
 	}
 	
