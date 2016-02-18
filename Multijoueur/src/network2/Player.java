fc5c4c5f64781ac14d52143318477f379bf80bcc
Koreuc <Koreuc@130.120.241.42>
2016-02-18 14:20:10 +0100

    ras

---
+++ Multijoueur/src/network2/Player.java
@@ -0,0 +1,43 @@
+package network2;
+import java.awt.Graphics;
+import java.io.Serializable;
+
+
+public class Player implements Serializable{
+
+	private String pseudo;
+	private int positionX;
+	private int positionY;
+	
+	public Player(String pseudo){
+		this.pseudo = pseudo;
+		this.initialiser();
+	}
+	
+	private void initialiser(){
+		this.positionX = 20; //BDD.getPositionX(this.pseudo);
+		this.positionY = 20; //BDD.getPositionY(this.pseudo);
+		System.out.println("Création de " + this);
+	}
+
+	public void afficherPersonnage(Graphics g){
+		g.fillRect(this.positionX, this.positionY, 20, 20);
+	}
+
+	public String getPseudo() {
+		return this.pseudo;
+	}
+
+	public int getPositionY() {
+		return this.positionY;
+	}
+	
+	public int getPositionX() {
+		return this.positionX;
+	}
+	
+	@Override
+	public String toString() {
+		return  this.pseudo + " (" + this.positionX	+ ";" + this.positionY + ")";
+	}
+}
