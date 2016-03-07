273622c7f8a01b34a47059015e4346f0b0820c42
Twarz <cortacero.k31130@gmail.com>
2016-03-07 15:17:13 +0100

    ras

--- TapTap/src/com/example/taptap/Authentification.java
+++ TapTap/src/com/example/taptap/Authentification.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Authentification extends Activity {
 
@@ -11,6 +16,7 @@ public class Authentification extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_authentification);
+		((Button)findViewById(R.id.button1)).setOnClickListener(new ConnexionListener(this));;
 	}
 
 	@Override
@@ -30,5 +36,41 @@ public class Authentification extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public class ConnexionListener implements OnClickListener {
+
+		private Activity activite;
+		
+		public ConnexionListener(Activity activite) {
+			this.activite = activite;
+		}
+		
+		@Override
+		public void onClick(View arg0) {
+			EditText et1 = ((EditText)this.activite.findViewById(R.id.editText1));
+			String pseudo = et1.getText().toString();
+			EditText et2 = ((EditText)this.activite.findViewById(R.id.editText2));
+			String mdp = et2.getText().toString();
+			
+			if (pseudo.equals("") && mdp.equals("")){
+				Toast.makeText(Authentification.this,"pseudo et mot de passe manquants", Toast.LENGTH_SHORT).show();
+			}
+			else if (pseudo.equals("")){
+				Toast.makeText(Authentification.this,"pseudo manquant", Toast.LENGTH_SHORT).show();
+			}else if (mdp.equals("")){
+				Toast.makeText(Authentification.this,"mot de passe manquant", Toast.LENGTH_SHORT).show();
+			}
+			else{
+				if (!pseudo.equals("Twarz") || !mdp.equals("123")){
+					Toast.makeText(Authentification.this,"pseudo ou mot de passe incorrect", Toast.LENGTH_LONG).show();
+				}
+				else{
+					Toast.makeText(Authentification.this,"Connexion", Toast.LENGTH_LONG).show();
+				}
+			}
+			
+		}
+
 	}
 }
