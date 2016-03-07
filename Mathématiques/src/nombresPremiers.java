94f5b8d1be1598ee7d74a439b64baa001f3463d8
Twarz <cortacero.k31130@gmail.com>
2016-03-07 15:24:03 +0100

    ras

--- Mathématiques/src/nombresPremiers.java
+++ Mathématiques/src/nombresPremiers.java
@@ -14,11 +14,14 @@ public class nombresPremiers {
 			}
 			nombre +=2;
 		}
+		int save = 0;
 		for (int i = 0; i < nombres.length; i ++ ){
-			String nbs = String.valueOf(nombres[i]);
-			String cs = String.valueOf(nbs.charAt(nbs.length()-1));
-			int c = Integer.valueOf(cs);
-			System.out.println("[" + (i+1) + "] --> " + nbs + " | " + c + " | " + (nombres[i]%c) + " | ");
+			int nb = 0;
+			if (i < nombres.length-1){
+				nb = nombres[i] + (nombres[i+1] - nombres[i])/2;
+				System.out.println("[" + (i+2) + "] --> " + nombres[i] + " | BLEU : "  +  nb + " | ECART : "  +  (nb-save)) ;
+			}
+			save = nb;
 		}
 	}
 	
