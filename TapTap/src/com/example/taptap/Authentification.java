aaacb734f9dde327188aa6e2af66f56b04d3b23c
Koreuc <Koreuc@130.120.210.188>
2016-03-07 15:18:21 +0100

    ras

--- TapTap/src/com/example/taptap/Authentification.java
+++ TapTap/src/com/example/taptap/Authentification.java
@@ -1,5 +1,9 @@
 package com.example.taptap;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+
 
 public class Authentification extends Activity {
 
@@ -9,22 +13,4 @@ public class Authentification extends Activity {
 		setContentView(R.layout.activity_authentification);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.authentification, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
